cf236abae110305016f2eb73767a555d07bedca1
Marcellinus Aditya Witarsah <68455552+marcellinus-witarsah@users.noreply.github.com>
2026-02-20 21:03:12 +0700

    Update iceberg_table_operations.py

--- src/utils/iceberg_table_operations.py
+++ src/utils/iceberg_table_operations.py
@@ -73,32 +73,3 @@ class IcebergTableOperations:
             logger.error(f"Error in get_latest_record: {e}")
             raise e
         return df
-
-    def get_earliest_record(
-        self,
-        table_name: str,
-        partition_cols: list,
-        order_cols: list,
-        filter_date_flag: bool = None,
-        start_date: str = None,
-        end_date: str = None,
-    ):
-        """
-        Get the earliest record for each partition in a table.
-
-        :param self: the IcebergTableOperations instance
-        :param table_name: name of the table to process
-        :param partition_cols: list of partition column names
-        :param order_cols: list of ordering column names
-        """
-        try:
-            logger.info(f"Getting earliest record from table {table_name}")
-            df = self.spark.table(table_name)
-            df = self.get_partitioned_row_number(df, partition_cols, order_cols, order="asc")
-            if filter_date_flag:
-                df = df.filter(col("load_dttm").between(start_date, end_date))
-            df = df.filter(col("row_number") == 1)
-        except Exception as e:
-            logger.error(f"Error in get_earliest_record: {e}")
-            raise e
-        return df
